ac7f533d320f06ccab5ddb98a6eed9a4aaaa41f0
Rugved Dhabale <99216117+RugvedDhabale@users.noreply.github.com>
2023-12-21 20:27:40 +0530

    Update QR code generatr .py

--- QR code generatr .py
+++ QR code generatr .py
@@ -11,7 +11,7 @@ from pyqrcode import QRCode
 
 
 # String which represents the QR code 
-s = "www.geeksforgeeks.org"
+s = "https://github.com/RugvedDhabale"
 
 # Generate QR code 
 url = pyqrcode.create(s) 
